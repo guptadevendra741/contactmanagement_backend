484500f80aef72524e85ad404fb78f431762e15b
guptadevendra741 <122438532+guptadevendra741@users.noreply.github.com>
2023-03-24 20:00:01 +0530

    Update connectDB.js
    Edited mongodb url

--- database/connectDB.js
+++ database/connectDB.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 mongoose.set("strictQuery",false)
 const connectDB=()=>{
-    mongoose.connect("mongodb+srv://recipe_app:0nqPG0uiWr2fzkus@cluster0.9temrwa.mongodb.net/?retryWrites=true&w=majority").then(()=>{
+    mongoose.connect("mongodb+srv:/ enter your own url").then(()=>{
         console.log("Database is connected!!");
     });
    
